dffb476f8090a37e5d0042d76cc920c6e5150d5e
VrezhBarseghyan <vrezh.barseghyann@gamil.com>
2020-11-14 20:55:54 +0400

    deploy

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'weatherapp',
+    'Weather.weatherapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
