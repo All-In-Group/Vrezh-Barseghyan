3ce3c9ce5b773bde19f9dc12b38707ed3f3a9241
VrezhBarseghyan <vrezh.barseghyann@gamil.com>
2020-11-14 20:33:07 +0400

    Deploy ready commit

--- Weather/weatherapp/tests.py
+++ Weather/weatherapp/tests.py
@@ -1,6 +1,1 @@
 # Create your tests here.
-import requests
-
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=8a8e8b25085fb1de4c10c0f61a51deb0'
-r = requests.get(url.format("V")).json()
-print(r)
