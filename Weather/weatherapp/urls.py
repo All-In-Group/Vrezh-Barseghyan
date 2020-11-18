44e599d8d9fe6f333971c37ae5d4d892c11b18b8
VrezhBarseghyan <vrezh.barseghyann@gamil.com>
2020-11-18 21:56:32 +0400

    api

--- Weather/weatherapp/urls.py
+++ Weather/weatherapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:info_id>', views.info, name='info'),
+    path('api/city', views.get_city_by_id, name='city')
 ]
--- Weather/weatherapp/views.py
+++ Weather/weatherapp/views.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .city_controller import get_all_city_names_starting_with_v
@@ -6,8 +8,8 @@ from .weather_config import weather_api_key
 
 
 def index(request):
-    cities = get_all_city_names_starting_with_v()
-    context = {'cities': cities}
+    city_name = get_all_city_names_starting_with_v()
+    context = {'cities': city_name}
     return render(request, 'weatherapp/index.html', context)
 
 
@@ -28,3 +30,21 @@ def info(request, info_id):
     return render(request, "weatherapp/info.html", {
         "city": city_weather
     })
+
+
+def get_city_by_id(request):
+    city_weather = {}
+    if request.method == "POST":
+        city_id = int(request.POST['city_id'])
+        city_name = get_all_city_names_starting_with_v()[city_id]
+        key = weather_api_key
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid={key}'
+
+        r = requests.get(url).json()
+        city_weather = {
+            'name': city_name,
+            'temperature': r['main']['temp'],
+            'humidity': r['main']['humidity'],
+            'wind': r['wind']['speed'],
+        }
+    return HttpResponse(json.dumps(city_weather))
